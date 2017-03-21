58514f86cc644992bf8285746271fbd8a22fd352
Leebabyfat <18222939567@163.com>
2017-03-21 11:08:28 +0800

    upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // 这是我们的玩家要躲避的敌人 
 var Enemy = function(x,y) {
     // 要应用到每个敌人的实例的变量写在这里
@@ -81,44 +81,7 @@ for(var i=0;i<6;i++){
 var player = new Player(200,300);
 // 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
 // 方法里面。你不需要再更改这段代码了。
-=======
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
->>>>>>> origin/master
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -128,8 +91,5 @@ document.addEventListener('keyup', function(e) {
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> origin/master
+
