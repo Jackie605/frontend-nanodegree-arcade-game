88fc38ea980d38334f425238ed2c43fc0a7a85df
Leebabyfat <18222939567@163.com>
2017-03-23 13:37:00 +0800

    fxed

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 // 这是我们的玩家要躲避的敌人 
 var Enemy = function(x,y) {
     // 要应用到每个敌人的实例的变量写在这里
@@ -7,7 +6,8 @@ var Enemy = function(x,y) {
     this.y = y;
     // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
     this.sprite = 'images/enemy-bug.png';
-    this.speed = Math.random()*100*2;
+    //给小虫子一个初始化的速度
+    this.speed = Math.random()*100*3;
 };
 
 // 此为游戏必须的函数，用来更新敌人的位置
@@ -16,64 +16,64 @@ Enemy.prototype.update = function(dt,x) {
     // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
     // 都是以同样的速度运行的
     this.x += dt * this.speed;
+    //Enemy一旦越出边界，那么就在起点处重复出现
     if(this.x >= 505){
         this.x = -30;
     }
 };
 
-// 此为游戏必须的函数，用来在屏幕上画出敌人，
+// 此为游戏必须的函数，用来在屏幕上画出敌人
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-Enemy.prototype.checkCollisions = function(player){
-    if(0<=play.x-this.x<=50 && Math.abs(player.y-this.y)<30)
-    {
-        console.log("loser");
-    }else{
-        console.log("player is safety!!");
-    }
-};
+
 // 现在实现你自己的玩家类
 // 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
 var Player = function(x,y){
     this.x = x;
     this.y = y;
     this.sprite = 'images/char-boy.png';
-};  
-Player.prototype.update = function(dt,x,y){
-    if(this.y<=30){
-        //设置字体样式
-        ctx.font = "80px Courier New";
-        //设置字体颜色
-        ctx.fillStyle = "Red";
-        //从坐标点(50,50)开始绘制文字
-        ctx.fillText("u win!!!", 50,50);
-        //setTimeout(1000);  
-        //this.x = 200;
-        //this.y = 300;
-        //ctx.clearRect(0,0,505,50);
-        
-    }  
 };
+//定义一个全局变量，为了使得玩家在每一毫秒刷新之前，能够不出现无法到达河岸，就弹出游戏胜利并回到原位的现象
+var count = 0;
+Player.prototype.update = function(dt){
+    console.log(this.y);
+
+    if(this.y === -11){
+       count++;
+       //经过测试，经过3ms后，就可以实现到达河岸，并网页输出胜利指示，确定后，即可回到原位。
+       if(count%3 === 2){
+            alert("U win！！ 点击确定后进入下一盘游戏");
+            this.x = 200;
+            this.y = 404;
+       }
+    }
+};
+//定义一个render渲染函数，用来在屏幕上画出玩家
 Player.prototype.render = function(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+//定义一个防止player越出画布边界的函数
 Player.prototype.handleInput = function(movement){
     switch(movement){
         case 'left':
+        //在不越出边界的前提下，如果能向左移，那就左移一个格子的width
         if(this.x>=0){
             this.x -= 101;
         }break;
         case 'right':
+        //在不越出边界的前提下，如果能向右移，那就右移一个格子的width
         if(this.x<=354){//404
             this.x += 101;
         }break;
         case 'up':
-        if(this.y>=50){//55
+        //在不越出边界的前提下，如果能向上移，那就上移一个格子的height
+        if(this.y>=72){//55
             this.y -= 83;
         }break;
         case 'down':
-        if(this.y<=300){//606
+        //在不越出边界的前提下，如果能向下移，那就下移一个格子的height
+        if(this.y<=321){//606
             this.y += 83;
         }break;
     }
@@ -81,17 +81,19 @@ Player.prototype.handleInput = function(movement){
 
 // 现在实例化你的所有对象
 // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
-// 把玩家对象放进一个叫 player 的变量里面
 var allEnemies = [];
+//定义6个Enemy，每行两个，每一行的初始位置是固定的
 for(var i=0;i<6;i++){
-    var bugs = new Enemy(-30,83*(i%3)+55);
+    var bugs = new Enemy(-30,83*(i%3)+72);
+    //将所有bugs都添加到allEnemies数组中
     allEnemies.push(bugs);
 };
 
-var player = new Player(200,300);
+// 把玩家对象放进一个叫 player 的变量里面
+var player = new Player(200,404);
+
 // 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
 // 方法里面。你不需要再更改这段代码了。
-
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -102,4 +104,3 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
